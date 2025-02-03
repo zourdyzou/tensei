0673470a5bdd565bd0f9ddee63eddd473ffe7c59
Zourdy M <zourdy@bereyziat.dev>
2025-02-03 21:53:58 +0100

    feat: add use action

---
+++ src/hooks/useAction.ts
@@ -0,0 +1,8 @@
+import Action from '../Action';
+
+const useAction =
+  <Args extends any[], Callback extends (...args: Args) => any>(action: Action<Args, Callback>) =>
+  (...args: Args) =>
+    action.call(...args);
+
+export default useAction;
